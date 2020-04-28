2bca2df2b435b1f91fe4fc5021ba02c7e6b918dd
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 15:56:10 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -2,5 +2,20 @@ import pytest
 import src.exercise
 
 def test_exercise():
-    #implement tests here
-    assert 0 == 0
+    input_values = ["data.txt"]
+    output = []
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    assert output == ["Name of the file:","lily, age: 3 years","anton, age: 5 years","levi, age: 4 years","amy, age: 1 year"]
